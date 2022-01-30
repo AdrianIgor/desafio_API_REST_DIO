70c5d2da8c50e65c11f4a3f243ed632c8cb1116f
Adrian Igor <desenevil07@gmail.com>
2022-01-29 21:02:44 -0300

    first commit

---
+++ src/main/java/com/organizacao/gestao/GestaoController.java
@@ -0,0 +1,16 @@
+package com.organizacao.gestao;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/ola")
+public class GestaoController {
+
+	@GetMapping
+	public String hello() {
+		return "Olá";
+	}
+	
+}

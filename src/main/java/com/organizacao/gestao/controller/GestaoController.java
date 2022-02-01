9574787773dca29ffd7bf1bf319d4f999ff682b6
Adrian Igor <desenevil07@gmail.com>
2022-02-01 20:06:24 -0300

    CRUD

--- src/main/java/com/organizacao/gestao/controller/GestaoController.java
+++ src/main/java/com/organizacao/gestao/controller/GestaoController.java
@@ -3,14 +3,22 @@ package com.organizacao.gestao.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.repository.query.Param;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.organizacao.gestao.entity.Address;
+import com.organizacao.gestao.entity.Person;
 import com.organizacao.gestao.entity.Profissional;
 import com.organizacao.gestao.repository.ProfissionalRepository;
+
+import lombok.NonNull;
 
 @RestController
 @RequestMapping("/")
@@ -28,10 +36,23 @@ public class GestaoController {
 		return repository.findAll();
 	}
 	
-	@PostMapping
+	@PostMapping("/post")
 	public String setProfissional(@RequestBody Profissional profissional) {
 		repository.save(profissional);
-		return "Oi";
+		return "Registro criado com sucesso!";
+	}
+	
+	@PutMapping("/put")
+	public String upProfissional(@RequestBody Profissional profissional) {
+		Profissional p = profissional;
+		repository.save(p);
+		return "Registro atualizado com sucesso!";
+	}
+	
+	@DeleteMapping("/delete")
+	public String delProfissional(@RequestBody Profissional profissional) {
+		repository.deleteById(profissional.getId());
+		return "Registro deletado com sucesso!";
 	}
 	
 }
--- src/main/java/com/organizacao/gestao/entity/Profissional.java
+++ src/main/java/com/organizacao/gestao/entity/Profissional.java
@@ -1,10 +1,12 @@
 package com.organizacao.gestao.entity;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.OneToOne;
 
@@ -29,10 +31,12 @@ public class Profissional {
 	@Column(nullable = false)
 	private double salario;
 	
-	@OneToOne
+	@OneToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "person_id", referencedColumnName = "id")
 	private Person person;
 	
-	@OneToOne
+	@OneToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "address_id", referencedColumnName = "id")
 	private Address address;
 	
 }
